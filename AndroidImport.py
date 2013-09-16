0cf8dfb70d6a5a52e391ffff55f2ea12ee41cfcf
Matt Oakes <oakesm9@googlemail.com>
2013-09-16 17:17:27 +0100

    Added the part of the script which actually adds the new imports when required

--- AndroidImport.py
+++ AndroidImport.py
@@ -10,12 +10,14 @@ plugin_path = os.path.dirname(os.path.abspath(__file__))
 json_file = open(plugin_path + '/classes.json')
 raw_class_list = json.loads(json_file.read())
 # Go through the class list and create a dictorary of the classes and packages
-class_list = dict()
+android_class_list = dict()
 for a_class in raw_class_list:
+    # Splits the package list and gets the last part which is the class
     split_label = a_class['label'].split('.')
     class_name = split_label[-1]
+    # Checks the first character is uppercase and discards if not, not a class
     if class_name[0].isupper():
-        class_list[class_name] = a_class['label']
+        android_class_list[class_name] = a_class['label']
 
 class AndroidImportCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -29,11 +31,49 @@ class AndroidImportCommand(sublime_plugin.TextCommand):
         for thing in tree.type_declarations:
             self.look_for_classes(thing)
 
-        # Print out the classes
-        print(self.classes)
+        # Filter to just the android classes
+        found_android_classes = set()
+        for found_class in self.classes:
+            if found_class in android_class_list:
+                found_android_classes.add(android_class_list[found_class])
+
+        # Look at all current imports
+        current_imports = set()
+        for an_import in tree.import_declarations:
+            current_imports.add(an_import.name.value)
+
+        # Get a set of imports which are still required
+        required_imports = set()
+        for package in found_android_classes:
+            if package not in current_imports:
+                required_imports.add(package)
+
+        # Create an import string to insert
+        import_string = ''
+        for package in required_imports:
+            import_string += 'import ' + package + ';\n'
+
+        # Find the last line of imports
+        lines = file_contents.split('\n')
+        got_to_imports = False
+        insert_point = None
+        for i, line in enumerate(lines):
+            if line.startswith('import '):
+                got_to_imports = True
+            elif not line.startswith('import ') and got_to_imports:
+                # Found last import line
+                insert_point = self.view.text_point(i, 0)
+                break
+        if insert_point is None:
+            self.view.insert(edit, self.view.text_point(1, 0), '\n')
+            insert_point = self.view.text_point(2, 0)
+
+        # Insert the new imports
+        self.view.insert(edit, insert_point, import_string)
 
         # Send a status message to show completion
-        sublime.status_message("Finished importing Android classes")
+        number_of_imports = str(len(required_imports))
+        sublime.status_message("Finished importing " + number_of_imports + " Android classes")
 
     # Only enabled for java files - Check this by looking for "java" in the current syntax name
     def is_enabled(self):
