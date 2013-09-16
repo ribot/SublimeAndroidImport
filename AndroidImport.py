9137ac9a0321414017af0f05c4b498876bc4f029
Matt Oakes <oakesm9@googlemail.com>
2013-09-16 16:35:06 +0100

    Get in and load a list of Android classes

--- AndroidImport.py
+++ AndroidImport.py
@@ -1,4 +1,21 @@
-import sublime, sublime_plugin, plyj, model
+import sublime, sublime_plugin, plyj, model, json, os
+
+class AndroidClass():
+    def __init__(self, name, package):
+        self.name = name
+        self.package = package
+
+# Load the list of classes
+plugin_path = os.path.dirname(os.path.abspath(__file__))
+json_file = open(plugin_path + '/classes.json')
+raw_class_list = json.loads(json_file.read())
+# Go through the class list and create a dictorary of the classes and packages
+class_list = dict()
+for a_class in raw_class_list:
+    split_label = a_class['label'].split('.')
+    class_name = split_label[-1]
+    if class_name[0].isupper():
+        class_list[class_name] = a_class['label']
 
 class AndroidImportCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -14,6 +31,9 @@ class AndroidImportCommand(sublime_plugin.TextCommand):
 
         # Print out the classes
         print(self.classes)
+
+        # Send a status message to show completion
+        sublime.status_message("Finished importing Android classes")
 
     # Only enabled for java files - Check this by looking for "java" in the current syntax name
     def is_enabled(self):
@@ -35,4 +55,3 @@ class AndroidImportCommand(sublime_plugin.TextCommand):
             elif hasattr(next_thing, '__iter__'):
                 for other_thing in next_thing:
                     self.look_for_classes(other_thing)
-
